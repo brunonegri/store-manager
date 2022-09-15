1e668801885a0d181cb98d19cb370e315760256a
Bruno Negri <b.negri_@hotmail.com>
2022-09-15 15:36:51 -0300

    Tests na model

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -5,7 +5,7 @@ const productsModel = require('../../../src/models/productsModel')
 const {connection} = require('../../../src/models/connection')
 const mock = require('./mockModel')
 
-describe('Testes na camada model', () => {
+describe('Test getAllProducts function', () => {
   before(async () => {
     sinon.stub(connection, 'execute').resolves([mock.allProducts])
   })
@@ -13,8 +13,35 @@ describe('Testes na camada model', () => {
   after(async () => {
     connection.execute.restore();
   })
-  it('Get Products', async () => {
+  it('[Model] Should return all Products', async () => {
     const result = await productsModel.getAllProducts()
     expect(result).to.be.deep.equal(mock.allProducts)
+  })
+})
+describe('Test getProductsById function', () => {
+  before(async () => {
+    sinon.stub(connection, 'execute').resolves([mock.oneProduct])
+  })
+  
+  after(async () => {
+    connection.execute.restore();
+  })
+  it('[Model] Should return one product with same ID', async () => {
+    const result = await productsModel.getProductsById(3)
+    expect(result).to.be.deep.equal(mock.allProducts[2])
+  })
+})
+
+describe('Test createProduct function', () => {
+  before(async () => {
+    sinon.stub(connection, 'execute').resolves([{insertId: 4}])
+  })
+  
+  after(async () => {
+    connection.execute.restore();
+  })
+  it('[Model] Should return new product ID :4', async () => {
+    const result = await productsModel.createProduct('SuperItem')
+    expect(result).to.be.deep.equal(4)
   })
 })
